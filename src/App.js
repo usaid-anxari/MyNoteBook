cb1e6b7dd17b080dc4047e9913de65a1f84ba45e
usaid ansari <usaidax@gmail.com>
2023-04-01 00:25:28 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {Home} from "./components/Home";
+import About from "./components/About";
 
 function App() {
-
+  return (
+    <>
+    <Router>
+        <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+    </Routes>
+    </Router>
+    </>
+  );
 }
 
 export default App;
